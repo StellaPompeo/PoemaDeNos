a597d36b8cd72e0b34e20baadbc58412623886fd
StellaPompeo <65262460+StellaPompeo@users.noreply.github.com>
2020-07-01 01:58:07 -0300

    Add files via upload
    incluso mais telas

--- js/funcoes.js
+++ js/funcoes.js
@@ -2,8 +2,4 @@ function mudaPoema(poema) {
     document.getElementById("icone").src = poema;
 }
 
-function calc_total() {
-    var qtd = parseInt(document.getElementById('cQtd').value);
-    tot = qtd * 1500;
-    document.getElementById('cTot').value = tot;
-  }
+
